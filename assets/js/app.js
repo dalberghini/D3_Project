7d6aa67cefe524390bab6c1e4e88917928434809
dalberghini <david.alberghini@gmail.com>
2021-04-11 18:32:03 -0600

    axes created

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,8 +20,28 @@ var svg = d3.select("#scatter")
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-d3.csv("assets/data/data.csv").then(d => {
-    console.log(d)
+d3.csv("assets/data/data.csv").then(data => {
+    console.log(data);
+    data.forEach( d => {
+    d.poverty =+ d.poverty;
+    });
+    var xScalePoverty = d3.scaleLinear()
+    .domain([d3.min(data, d=> d.poverty), d3.max(data, d=>d.poverty)])
+    .range([0, chartWidth]);
+
+    var yScaleObesity = d3.scaleLinear()
+        .domain([d3.min(data, d=> d.obesity), d3.max(data, d=> d.obesity)])
+        .range([chartHeight, 0]);
+
+    var bottomAxis = d3.axisBottom(xScalePoverty);
+    var leftAxis = d3.axisLeft(yScaleObesity);
+    
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
+    chartGroup.append("g")
+        .call(leftAxis);
+
 }).catch(function(error) {
     console.log(error);
 });
